4fc296c12ed57b29e8a563e9ccbd5792de4f6284
Jensen <jensechu@gmail.com>
2012-12-17 23:33:20 -0800

    When the user doubles clicks, the names refresh.

--- js/namer.js
+++ js/namer.js
@@ -1,8 +1,9 @@
 // namer.js, for those times when you're like 'Shit I need a character name.'
 
-function name_randomizer(nameType){
+function name_randomizer(nameType) {
     
     // Grabs the names from the JSON file and assigns them to a variable.
+    // Passes back a random name from the unpacked list.
     $.getJSON("data/names.json", function(data) {	
 	var allNames = {}
 	allNames["prefixes"] = data.prefixes;
@@ -10,13 +11,8 @@ function name_randomizer(nameType){
 
 	var randomName = allNames[nameType][Math.floor(Math.random() * allNames[nameType].length)];
 
-<<<<<<< HEAD
-	var allSuffixes = data.suffixes;
-	console.log(allSuffixes);
-=======
 	console.log(randomName);
 	
->>>>>>> randomizer
     });
 };
 
@@ -29,9 +25,19 @@ $(document).ready(function() {
 	name_randomizer(nameType);
     });
 
-    $(".refresh").click(function() {
+    //Click each name to run the name_randomizer() on 'em.
+    function refresh_names() {
 	$("#generator-wrap h1").each(function(index) {
 	    $(this).click();
 	});
+    };
+
+    //Refresh names onLoad
+    refresh_names();
+
+    //Refresh names on double click.
+    $("#generator-wrap h1").dblclick(function() {
+	refresh_names();
     });
+    
 });

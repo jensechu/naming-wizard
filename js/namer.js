7e7002564e0f6d601ebfc3be089d4a747de0ecf7
Jensen <jensechu@gmail.com>
2012-12-17 23:20:30 -0800

    Added a hash table for each name type in the JSON.

--- js/namer.js
+++ js/namer.js
@@ -1,19 +1,37 @@
 // namer.js, for those times when you're like 'Shit I need a character name.'
 
-function name_randomizer(){
-    console.log("Name randomizer is running.");
+function name_randomizer(nameType){
     
-    $.getJSON("data/names.json", function(data) {
-    
-	var allPrefixes = data.prefixes;
-	console.log(allPrefixes);
+    // Grabs the names from the JSON file and assigns them to a variable.
+    $.getJSON("data/names.json", function(data) {	
+	var allNames = {}
+	allNames["prefixes"] = data.prefixes;
+	allNames["suffixes"] = data.suffixes;
 
+	var randomName = allNames[nameType][Math.floor(Math.random() * allNames[nameType].length)];
+
+<<<<<<< HEAD
 	var allSuffixes = data.suffixes;
 	console.log(allSuffixes);
+=======
+	console.log(randomName);
+	
+>>>>>>> randomizer
     });
 };
 
+$(document).ready(function() {
 
-	      
+    //Grab the class (prefix or suffix, but flexible for more types).
+    // Run the randomizer with the name type.
+    $("#generator-wrap h1").click(function() {
+	var nameType = $(this).attr("class");
+	name_randomizer(nameType);
+    });
 
-name_randomizer();
+    $(".refresh").click(function() {
+	$("#generator-wrap h1").each(function(index) {
+	    $(this).click();
+	});
+    });
+});

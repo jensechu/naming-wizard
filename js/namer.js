c48ffa75f8af93406e21ae8e254917881845febe
Jensen <jensechu@gmail.com>
2012-12-17 21:35:43 -0800

    JSON formatting whitespace.

--- js/namer.js
+++ js/namer.js
@@ -1,3 +1,19 @@
 // namer.js, for those times when you're like 'Shit I need a character name.'
 
+function name_randomizer(){
+    console.log("Name randomizer is running.");
+    
+    $.getJSON("data/names.json", function(data) {
+    
+	var allPrefixes = data.prefixes;
+	console.log(allPrefixes);
 
+	var allSuffixes = date.suffixes;
+	console.log(allSuffixes);
+    });
+};
+
+
+	      
+
+name_randomizer();
